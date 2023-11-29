d4e6ecfdf182b7fe2781f88161ad79de2e508183
fancy-gems <150167679+fancy-gems@users.noreply.github.com>
2023-11-29 12:04:54 +0800

    commented the heroku code

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import 'dotenv/config';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const app = express()
+/*const app = express()
 const port = process.env.PORT || 3000
-app.use(express.static(path.join(__dirname, "2311 TechUp Assignment")));
+app.use(express.static(path.join(__dirname, "2311 TechUp Assignment")));*/
