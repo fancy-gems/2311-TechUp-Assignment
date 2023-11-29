6bfa8cfbf90ae9057d6f4ad85d6d645b895fd64c
fancy-gems <smart.treetop@gmail.com>
2023-11-29 09:21:11 +0800

    some edits to index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-var PORT = process.env.PORT || 3000;
 import express from "express";
 import { MongoClient, ServerApiVersion }from "mongodb";
 import dotenv from "dotenv";
 import 'dotenv/config';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.static(path.join(__dirname, "2311 TechUp Assignment")));
